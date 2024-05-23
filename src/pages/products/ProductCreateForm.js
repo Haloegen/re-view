59b264baaa7fde16898b8e8483ce36c24e6cc0c9
Josh Lee <JoshCliffLee@gmail.com>
2024-05-23 21:08:48 +0000

    add profileeditform dropdown menu to allow users to edit profile

--- src/pages/products/ProductCreateForm.js
+++ src/pages/products/ProductCreateForm.js
@@ -1,6 +1,5 @@
 import React, { useState, useRef } from "react";
-import Upload from "../../assets/upload.png"
-
+import Upload from "../../assets/upload.png";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
@@ -26,9 +25,8 @@ function ProductCreateForm() {
   });
 
   const { title, content, image, price } = productData;
-const imageInput = useRef(null)
-const history = useHistory()
-
+  const imageInput = useRef(null);
+  const history = useHistory();
 
   const handleChange = (event) => {
     setProductData({
@@ -47,7 +45,6 @@ const history = useHistory()
     }
   };
 
-
   const handleSubmit = async (event) => {
     event.preventDefault();
     const formData = new FormData();
@@ -55,7 +52,7 @@ const history = useHistory()
     formData.append("title", title);
     formData.append("content", content);
     formData.append("image", imageInput.current.files[0]);
-    formData.append('price', price);
+    formData.append("price", price);
 
     try {
       const { data } = await axiosReq.post("/products/", formData);
@@ -68,121 +65,122 @@ const history = useHistory()
     }
   };
 
-
-
   const textFields = (
-    <div className="text-center">
+    <div className="text-center mt-3">
       <Form.Group>
-        <Form.Label>Title</Form.Label>
+        <Form.Label className="h5">Title</Form.Label>
         <Form.Control
           type="text"
           name="title"
           value={title}
           onChange={handleChange}
+          className="mb-3"
         />
       </Form.Group>
       {errors?.title?.map((message, idx) => (
-        <Alert variant="warning" key={idx}>
+        <Alert variant="danger" key={idx}>
           {message}
         </Alert>
       ))}
+            <Form.Group>
+        <Form.Label className="h5">Price</Form.Label>
+        <Form.Control
+          type="number"
+          name="price"
+          value={price}
+          onChange={handleChange}
+          className="mb-3"
+        />
+      </Form.Group>
+      {errors?.price?.map((message, idx) => (
+        <Alert variant="danger" key={idx}>
+          {message}
+        </Alert>
+      ))}
+
       <Form.Group>
-        <Form.Label>Content</Form.Label>
+        <Form.Label className="h5">Content</Form.Label>
         <Form.Control
           as="textarea"
           rows={6}
           name="content"
           value={content}
           onChange={handleChange}
+          className="mb-3"
         />
       </Form.Group>
-      {errors?.title?.map((message, idx) => (
-        <Alert variant="warning" key={idx}>
-          {message}
-        </Alert>
-      ))}
-      <Form.Group>
-        <Form.Label>
-          Price
-        </Form.Label>
-        <Form.Control
-        type="num"
-        name="price"
-        value={price}
-        onChange={handleChange}
-        />
-      </Form.Group>
-      {errors?.title?.map((message, idx) => (
-        <Alert variant="warning" key={idx}>
+      {errors?.content?.map((message, idx) => (
+        <Alert variant="danger" key={idx}>
           {message}
         </Alert>
       ))}
 
       <Button
-        className={`${btnStyles.Button} ${btnStyles.Blue}`}
+        className={`${btnStyles.Button} ${btnStyles.Blue} me-2`}
         onClick={() => history.goBack()}
       >
-        cancel
+        Cancel
       </Button>
       <Button className={`${btnStyles.Button} ${btnStyles.Blue}`} type="submit">
-        create
+        Create
       </Button>
     </div>
   );
 
   return (
     <Form onSubmit={handleSubmit}>
-      <Row>
-        <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
-          <Container
-            className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
-          >
-            <Form.Group className="text-center">
-              {image ? (
-                <>
-                  <figure>
-                    <Image className={appStyles.Image} src={image} rounded />
-                  </figure>
-                  <div>
-                    <Form.Label
-                      className={`${btnStyles.Button} ${btnStyles.Blue} btn`}
-                      htmlFor="image-upload"
-                    >
-                      Change the image
-                    </Form.Label>
-                  </div>
-                </>
-              ) : (
-                <Form.Label
-                  className="d-flex justify-content-center"
-                  htmlFor="image-upload"
-                >
-                                    <Asset
-                    src={Upload}
-                    message="Click or tap to upload an image"
-                  />
-                </Form.Label>
-              )}
+      <Container className="my-4">
+        <Row className="justify-content-center">
+          <Col md={8} lg={6}>
+            <Container
+              className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
+            >
+              <Form.Group className="text-center">
+                {image ? (
+                  <>
+                    <figure>
+                      <Image className={appStyles.Image} src={image} rounded />
+                    </figure>
+                    <div>
+                      <Form.Label
+                        className={`${btnStyles.Button} ${btnStyles.Blue} btn`}
+                        htmlFor="image-upload"
+                      >
+                        Change the image
+                      </Form.Label>
+                    </div>
+                  </>
+                ) : (
+                  <Form.Label
+                    className="d-flex justify-content-center"
+                    htmlFor="image-upload"
+                  >
+                    <Asset
+                      src={Upload}
+                      message="Click or tap to upload an image"
+                    />
+                  </Form.Label>
+                )}
 
-              <Form.File
-                id="image-upload"
-                accept="image/*"
-                onChange={handleChangeImage}
-                ref={imageInput}
-              />
-            </Form.Group>
-            {errors?.image?.map((message, idx) => (
-              <Alert variant="warning" key={idx}>
-                {message}
-              </Alert>
-            ))}
-            <div className="d-md-none">{textFields}</div>
-          </Container>
-        </Col>
-        <Col md={5} lg={4} className="d-none d-md-block p-0 p-md-2">
-          <Container className={appStyles.Content}>{textFields}</Container>
-        </Col>
-      </Row>
+                <Form.File
+                  id="image-upload"
+                  accept="image/*"
+                  onChange={handleChangeImage}
+                  ref={imageInput}
+                />
+              </Form.Group>
+              {errors?.image?.map((message, idx) => (
+                <Alert variant="danger" key={idx}>
+                  {message}
+                </Alert>
+              ))}
+            </Container>
+            <Container className={`${appStyles.Content} mt-4`}>
+              {textFields}
+            </Container>
+          </Col>
+        </Row>
+      </Container>
     </Form>
   );
 }
--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -23,6 +23,7 @@ import {
   useSetProfileData,
 } from "../../contexts/ProfileDataContext";
 import { Button, Image } from "react-bootstrap";
+import { ProfileEditDropdown } from "../../components/MoreDropdown";
 
 function ProfilePage() {
   const [hasLoaded, setHasLoaded] = useState(false);
@@ -57,6 +58,7 @@ function ProfilePage() {
 
   const mainProfile = (
     <>
+    {profile?.is_owner && <ProfileEditDropdown id={profile?.id} />}
       <Row noGutters className="px-3 text-center">
         <Col lg={3} className="text-lg-left">
           <Image
